bfca0fb24250610b178e0e610bd35af6a8561287
webtohouse <webtohouse@gmail.com>
2017-09-07 18:57:10 +0900

    이벤트 이해하기

--- NodeExample1/ch04_test4.js
+++ NodeExample1/ch04_test4.js
@@ -2,7 +2,7 @@ var Calc = require('./calc3');
 
 var calc1 = new Calc();
 console.log('Calc 에 stop 이벤트 전달함.');
-calc1.emit('stop');
+calc1.emit('stop'); // 이벤트 전달.
 
 
 var result = calc1.add( 10, 10 );
