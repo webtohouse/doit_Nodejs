cbb6ba6c92b5100714319e72a0dc0d9ec1a739d2
webtohouse <webtohouse@gmail.com>
2017-10-10 17:53:40 +0900

    라우터미들웨어

--- ExpressExample/app8.js
+++ ExpressExample/app8.js
@@ -11,6 +11,7 @@
 
 var express = require('express');
 var http = require('http');
+var static = require('serve-static');
 var path = require('path');
 var bodyParser = require('body-parser');
 
@@ -22,9 +23,9 @@ var server = http.createServer(app).listen(app.get('port'), function() {
     console.log('익스프레스로 웹서버 실행함 : ' + app.get('port'));
 });
 
-
 // static 미들웨어 설정(특정 폴더의 파일들을 특정 패스로 접근할 수 있도록 만들어 줌.)
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use('/public', express.static(path.join(__dirname, 'public')));
+app.use(static(path.join(__dirname, 'public')));
 /*
 * Express 앱을 다른 디렉토리에서 실행하는 경우에는 다음과 같이 제공하기 원하는 디렉토리의 절대 경로를 사용하는 것이 더 안전
 * public 폴더 안에 있는 파일을 /static 패스로 접근하도록 한다.
@@ -33,10 +34,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // body-parser 미들웨어 설정
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
-// POST 방식의 특정 path 요청처리.
-app.post('/process/login', function(req, res) {
+var router = express.Router();
 
+router.route('/process/login').post(function(req, res) {
     var requestPath = req.path; // 요청 path 확인
     var paramid = req.body.id;
     var paramPassword = req.body.password;
@@ -50,6 +52,40 @@ app.post('/process/login', function(req, res) {
     res.write('<div><p>Param PASSWORD : ' + paramPassword + '</p></div>');
     res.end();
 });
+
+// router.post('/process/login', function(req, res) {
+//     var requestPath = req.path; // 요청 path 확인
+//     var paramid = req.body.id;
+//     var paramPassword = req.body.password;
+//
+//     console.log(requestPath + ' 요청을 처리함.');
+//     res.writeHead('200', {'Content-Type':'text/html;charset=utf=8'});
+//     res.write('<h1>로그인 성공!!.</h1>');
+//     res.write("<img src='/images/login.png' width='100'>");
+//
+//     res.write('<div><p>Param ID : ' + paramid + '</p></div>');
+//     res.write('<div><p>Param PASSWORD : ' + paramPassword + '</p></div>');
+//     res.end();
+// });
+
+app.use('/', router);
+
+// POST 방식의 특정 path 요청처리.
+// app.post('/process/login', function(req, res) {
+//
+//     var requestPath = req.path; // 요청 path 확인
+//     var paramid = req.body.id;
+//     var paramPassword = req.body.password;
+//
+//     console.log(requestPath + ' 요청을 처리함.');
+//     res.writeHead('200', {'Content-Type':'text/html;charset=utf=8'});
+//     res.write('<h1>로그인 성공!!.</h1>');
+//     res.write("<img src='/images/login.png' width='100'>");
+//
+//     res.write('<div><p>Param ID : ' + paramid + '</p></div>');
+//     res.write('<div><p>Param PASSWORD : ' + paramPassword + '</p></div>');
+//     res.end();
+// });
 
 // 라우터로 설정된 이외의 path로 접근 했을 때 처리.
 app.all('*', function(req, res) {
